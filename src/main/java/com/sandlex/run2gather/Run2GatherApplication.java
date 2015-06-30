329fba88497f3a9f70916278e05a595e3003d331
Alexey Peskov <a.peskov@secondfloor.nl>
2015-06-30 16:23:52 +0200

    Playing with OAuth2

--- src/main/java/com/sandlex/run2gather/Run2GatherApplication.java
+++ src/main/java/com/sandlex/run2gather/Run2GatherApplication.java
@@ -1,20 +1,35 @@
 package com.sandlex.run2gather;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author apeskov
  */
-@RestController
+//@RestController
 @EnableAutoConfiguration
+@EnableOAuth2Client
+@ComponentScan
+@Configuration
 public class Run2GatherApplication {
+
+    @Autowired
+    private OAuth2RestTemplate oAuth2RestTemplate;
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return oAuth2RestTemplate.getAccessToken().getValue();
     }
 
     public static void main(String[] args) throws Exception {
