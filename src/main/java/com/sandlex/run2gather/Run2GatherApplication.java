b3ffa0619c03a764b39c82e72b5cab670721baf4
Alexey Peskov <a.peskov@secondfloor.nl>
2015-07-02 14:36:23 +0200

    OAuth2 client can connect to RunKeeper

--- src/main/java/com/sandlex/run2gather/Run2GatherApplication.java
+++ src/main/java/com/sandlex/run2gather/Run2GatherApplication.java
@@ -1,27 +1,19 @@
 package com.sandlex.run2gather;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author apeskov
  */
-//@RestController
 @EnableAutoConfiguration
 @EnableOAuth2Client
-@ComponentScan
-@Configuration
+@RestController
 public class Run2GatherApplication {
 
     @Autowired
@@ -36,4 +28,36 @@ public class Run2GatherApplication {
         SpringApplication.run(Run2GatherApplication.class, args);
     }
 
+//    @Value("${oauth.resource:https://runkeeper.com}")
+//    private String baseUrl;
+//
+//    @Value("${oauth.authorize:https://runkeeper.com/apps/authorize}")
+//    private String authorizeUrl;
+//
+//    @Value("${oauth.token:https://runkeeper.com/apps/token}")
+//    private String tokenUrl;
+//
+//    @Autowired
+//    private OAuth2RestOperations restTemplate;
+//
+//    @RequestMapping("/")
+//    public List<Map<String,?>> home() {
+//        @SuppressWarnings("unchecked")
+//        List<Map<String,?>> result = restTemplate.getForObject(baseUrl + "/admin/beans", List.class);
+//        return result;
+//    }
+//
+//    @Bean
+//    public OAuth2RestOperations restTemplate(OAuth2ClientContext oauth2ClientContext) {
+//        return new OAuth2RestTemplate(resource(), oauth2ClientContext);
+//    }
+//
+//    @Bean
+//    protected OAuth2ProtectedResourceDetails resource() {
+//        AuthorizationCodeResourceDetails resource = new AuthorizationCodeResourceDetails();
+//        resource.setAccessTokenUri(tokenUrl);
+//        resource.setUserAuthorizationUri(authorizeUrl);
+//        resource.setClientId("my-trusted-client");
+//        return resource ;
+//    }
 }
